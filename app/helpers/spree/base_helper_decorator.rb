98b0f6caf3d38caf4b5e534e656df467e4d760bc
Romain Richard <romain.g.richard@gmail.com>
2017-04-25 19:16:21 +0200

    index done, need to implement caroussel and then 100% complete for index

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,5 +1,35 @@
 module Spree::BaseHelper
-  def logo(image_path = 'spree/frontend/logo.svg', img_option: {})
+  def logo(image_path = 'spree/frontend/Logo2.png', img_option: {})
     link_to image_tag(image_path, img_option), spree.root_path
+  end
+
+  def nav_tree(root_taxon, current_taxon, max_level = 1)
+    return '' if max_level < 1 || root_taxon.children.empty?
+    content_tag :ul, class: 'taxons-list' do
+      taxons = root_taxon.children.map do |taxon|
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'current' : nil
+        content_tag :li, class: "#{css_class} taxon" do
+         link_to(taxon.name, seo_url(taxon)) +
+           taxons_tree(taxon, current_taxon, max_level - 1) +
+           "|"
+        end
+      end
+      safe_join(taxons, "\n")
+    end
+  end
+
+  def link_to_cart(text = nil)
+    text = text ? h(text) : Spree.t(:cart)
+    css_class = nil
+
+    if simple_current_order.nil? || simple_current_order.item_count.zero?
+      text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
+      css_class = 'empty'
+    else
+      text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
+      css_class = 'full'
+    end
+
+    link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}"
   end
 end
