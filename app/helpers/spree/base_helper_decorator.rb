cd718a57231859c162a276a1a3b9e6d9eeeeaa58
Romain Richard <romain.g.richard@gmail.com>
2017-04-26 11:36:46 +0200

    currencies and bug linked to @taxonomies fixed

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -17,19 +17,4 @@ module Spree::BaseHelper
       safe_join(taxons, "\n")
     end
   end
-
-  def link_to_cart(text = nil)
-    text = text ? h(text) : Spree.t(:cart)
-    css_class = nil
-
-    if simple_current_order.nil? || simple_current_order.item_count.zero?
-      text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
-      css_class = 'empty'
-    else
-      text = "#{text}: (#{simple_current_order.item_count})  <span class='amount'>#{simple_current_order.display_total.to_html}</span>"
-      css_class = 'full'
-    end
-
-    link_to text.html_safe, spree.cart_path, class: "cart-info #{css_class}"
-  end
 end
